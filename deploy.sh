70be54e9d9f4b34cd5c6fc3ba7b00300fa446370
Richard Hudson <rhudson@issuma.com>
2018-11-29 16:58:15 -0500

    fake change forcing build

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,3 @@ kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=rhtwl/multi-server:$SHA
 kubectl set image deployments/client-deployment client=rhtwl/multi-client:$SHA
 kubectl set image deployments/client-workder worker=rhtwl/multi-worker:$SHA
-
